6dc31c3937cf6d05c2feefb497697a7dcaa6add1
Daniel Vidal <dvidals@zoho.com>
2024-07-19 14:47:33 +0200

    First commit

---
+++ build/turtlebot3_example/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/tumwfml-ubunt6/ros2_ws/src/turtlebot3_ws/install/turtlebot3_example'
